e83c153bf6aff777662b0cdc80f79a77bd3a15e6
priyanshuthakur679-oss <priyanshuthakur679@gmail.com>
2026-01-13 21:34:15 +0530

    hardcoded url

--- src/index.js
+++ src/index.js
@@ -18,9 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL
-        : "http://localhost:5173",
+     "https://whatsapp-frontend-navy.vercel.app",
     credentials: true,
   })
 );
