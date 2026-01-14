f49828413e0caeb24bb92240e75d320181b0d203
priyanshuthakur679-oss <priyanshuthakur679@gmail.com>
2026-01-14 21:59:26 +0530

    resolve unauth

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // must be true on HTTPS
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: process.env.NODE_ENV === "production", // true only in production
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "None", // Always "None" to allow cross-origin cookies
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
