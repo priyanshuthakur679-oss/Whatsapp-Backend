b3d256198b63023d0ab39c0ca74e803aba7e977e
priyanshuthakur679-oss <priyanshuthakur679@gmail.com>
2026-01-14 22:06:47 +0530

    update on token

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true only in production
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "None", // Always "None" to allow cross-origin cookies
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // "Lax" for dev, "None" for production
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
